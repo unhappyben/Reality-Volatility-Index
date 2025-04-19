ac04173c0b7da2750bff23b2caf7b56826213a33
unhappyben <ben.renshaw@hotmail.com>
2025-04-19 15:57:33 +0200

    update lines, and other stuff

--- scripts/fetch_and_transform_rvi.js
+++ scripts/fetch_and_transform_rvi.js
@@ -4,10 +4,8 @@ import fetch from "node-fetch";
 import dotenv from "dotenv";
 dotenv.config();
 
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const ROOT_DIR = process.cwd();
+
 
 const API_URL = "https://api.data.adj.news/api/markets";
 
